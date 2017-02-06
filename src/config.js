492e191de064163c9c9f0bc861ffdb10ccc5ba22
Stas Altsev <alstas@gmail.com>
2017-02-06 15:14:52 +0300

    add config and mapping for testrail statuses ids

---
+++ src/config.js
@@ -0,0 +1,17 @@
+
+/*
+ status_id	int	The ID of the test status. The built-in system statuses have the following IDs:
+ 1	Passed
+ 2	Blocked
+ 3	Untested (not allowed when adding a result)
+ 4	Retest
+ 5	Failed
+ You can get a full list of system and custom statuses via get_statuses.
+ */
+module.exports.mapMocha2TestRailStatuses = {
+  'failed': 5,
+  'passed': 1,
+};
+
+module.exports.TESTRAIL_TESTRUN_ID = 'TESTRAIL_TESTRUN_ID';
+
