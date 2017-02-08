14fb1e9b12c0e0b9bdc861e73e59b9f2e746ad15
Stas Altsev <alstas@gmail.com>
2017-02-08 15:09:48 +0300

    * reformatted testrail.js

--- src/testrail.js
+++ src/testrail.js
@@ -1,7 +1,8 @@
 const postRequest = require('./utils').postRequest;
 
-module.exports.addResultsForCases = function addResultsForCases(results, config) {
-  const url = `https://${config.user}:${config.pass}@webapp20.testrail.net/index.php?/api/v2/add_results_for_cases/${config.runId}`;
+module.exports.addResultsForCases = function addResultsForCases(results, options) {
+  const url = `https://${options.user}:${options.pass}@webapp20.testrail.net/index.php?/api/v2/add_results_for_cases/${options.runId}`;
   const data = { results: results };
+
   return postRequest(url, data);
 };
